7f8a7c8e971d84bceff3808cb112e86dc69b5ae5
James Lee <jimhlee23@gmail.com>
2024-03-18 20:56:07 -0400

    finish first 4 problems

--- async.js
+++ async.js
@@ -2,63 +2,73 @@
 
 const BASE_URL = 'http://numbersapi.com';
 
-async function showNumberTrivia() {
+async function showNumberTrivia(num) {
   // get response
   const response = await fetch(
-    `${BASE_URL}/42?json`);
+    `${BASE_URL}/${num}?json`);
   // {headers: {'Content-Type': 'application/json'}});
-  console.log(response);
+  // console.log(response);
   // parse response
   const responseData = await response.json();
-  console.log(responseData);
   // log data
   console.log(responseData.text);
 }
 
+async function showNumberRace(nums) {
+  // call this promises instead, be explicit
+  const requests = nums.map((num) => {
+    return fetch(`${BASE_URL}/${num}?json`);
+  });
 
+  // const num1 = fetch(`${BASE_URL}/12?json`);
+  // const num2 = fetch(`${BASE_URL}/7?json`);
+  // const num3 = fetch(`${BASE_URL}/19?json`);
+  // const num4 = fetch(`${BASE_URL}/5?json`);
 
-
-await showNumberTrivia();
-
-
-async function showNumberRace() {
-  const num1 = fetch(`${BASE_URL}/12?json`);
-  const num2 = fetch(`${BASE_URL}/7?json`);
-  const num3 = fetch(`${BASE_URL}/19?json`);
-  const num4 = fetch(`${BASE_URL}/5?json`);
-
-  const answerPromise = await Promise.race([num1, num2, num3, num4]);
+  // call this winner or similar
+  const answerPromise = await Promise.race(requests);
   const responseData = await answerPromise.json();
   console.log(await responseData.text);
 }
 
-await showNumberRace();
+async function showNumberAll(nums) {
+  const answerPromise = nums.map((num) => {
+    return fetch(`${BASE_URL}/${num}?json`);
+  });
 
+  // const num1 = fetch(`${BASE_URL}/12?json`);
+  // const num2 = fetch(`${BASE_URL}/7?json`);
+  // const num3 = fetch(`${BASE_URL}/19?json`);
+  // const num4 = fetch(`${BASE_URL}/WRONG?json`);
 
-async function showNumberAll() {
-  const num1 = fetch(`${BASE_URL}/12?json`);
-  const num2 = fetch(`${BASE_URL}/7?json`);
-  const num3 = fetch(`${BASE_URL}/19?json`);
-  const num4 = fetch(`${BASE_URL}/WRONG?json`);
-
-  let results = await Promise.allSettled([num1, num2, num3, num4]);
-  console.log(results[0]);
+  let results = await Promise.allSettled(answerPromise);
 
   const successArray = [];
   const failureArray = [];
 
   for (let result of results) {
-      if (result.value.ok) {
-        console.log("result=", result);
-        const response = await result.value.json();
-        successArray.push(response.text);
-      } else {
-        failureArray.push(result.value.statusText);
-      }
+    if (result.value.ok) {
+      // console.log("result=", result);
+      const response = await result.value.json();
+      successArray.push(response.text);
+    } else {
+      failureArray.push(result.value.statusText);
+    }
   }
   console.log(successArray);
   console.log(failureArray);
 
 }
 
-await showNumberAll();
+
+async function main() {
+  const first = await showNumberTrivia(12);
+  // console.log(first);
+  const second = await showNumberRace([1, 2, 3, 4]);
+  // console.log(second);
+  const third = await showNumberAll([12, 7, 30, 'wrong']);
+  // console.log(third);
+
+}
+
+main();
